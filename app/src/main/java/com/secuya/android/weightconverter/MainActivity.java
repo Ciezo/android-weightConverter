d0f6e528c74f16cc153d4199ac6fe5a20cac3ea4
Cloyd Van <abcd123.letters@gmail.com>
2023-01-03 13:01:11 +0800

    Commit project to remote

---
+++ app/src/main/java/com/secuya/android/weightconverter/MainActivity.java
@@ -0,0 +1,82 @@
+package com.secuya.android.weightconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    double conversion_rate = 2.2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+         * Set up the custom logo
+         */
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        /**
+         * Instantiate the Views components
+         */
+        EditText weight = (EditText) findViewById(R.id.txtWeight);      // Number is an input
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        RadioButton lbToKilo = (RadioButton) findViewById(R.id.radLbToKilo);
+        RadioButton kiloToLb = (RadioButton) findViewById(R.id.radKiloToLb);
+        TextView result = (TextView) findViewById(R.id.txtResult);
+        Button btnConvert = (Button) findViewById(R.id.btnConvert);
+
+        /**
+         * @Todo Add event handling to the convert button
+         */
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Fetch the weight entered by the user as an input
+                double tmp_weight = Double.parseDouble(weight.getText().toString());
+
+                // Check if any of the buttons inside the radio group is checked (selected)
+                if (radioGroup.getCheckedRadioButtonId() == -1) {
+                    // no radio buttons are selected
+                }
+
+                else {
+                    if (lbToKilo.isChecked()) {
+                        if (tmp_weight <= 500) {
+                            // @todo Pounds to Kilograms convert
+                            double conv_weight = tmp_weight/conversion_rate;
+                            result.setText("Kilograms: "+ conv_weight);
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Pounds must be less than 500", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    else if (kiloToLb.isChecked()) {
+                        if (tmp_weight <= 225) {
+                            // @todo Kilograms to Pounds convert
+                            double conv_weight = tmp_weight*conversion_rate;
+                            result.setText("Pounds: "+ conv_weight);
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Kilograms must be less than 225", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+
+            }
+        });
+
+    }
+}
